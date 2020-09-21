c49eadb812d1d55f8f4716dc98e07bbde861bfb8
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2020-09-21 10:55:29 +0100

    changes

--- src/Views/components/CkeditorBuilderJs.blade.php
+++ src/Views/components/CkeditorBuilderJs.blade.php
@@ -1,8 +1,6 @@
 
 {{-- need for the editor login --}}
 <script src="{{ asset('vendor/Onix/js/app.js') }}"></script>
-{{-- need for the editor plugins  --}}
-<script src="{{ asset('vendor/Onix/grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.js') }}"></script>
 
 {{-- in here we start the editor with our setup --}}
 <script>
