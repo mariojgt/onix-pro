5d5cec5f6b41a3eba098cee5a2ee537c85f20d55
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2020-09-22 16:34:47 +0100

    quick changes

--- src/Views/builder/jsGrape.blade.php
+++ src/Views/builder/jsGrape.blade.php
@@ -1,10 +1,13 @@
-
-
-    {{-- you can you a push in the componente if need to stack your css or js --}}
-    <x-onix::BuilderCss />
-        <x-onix::grapeBuilder>
-            {{-- set your style like boostrap or this case tailwind so the brower can render inside the componente --}}
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        </x-onix::grapeBuilder>
-    {{-- you can you a push in the componente if need to stack your css or js --}}
-    <x-onix::GrapeBuilderJs />
+<x-onix::grapeBuilder
+    {{-- required --}}
+    pushLocationCss="css"
+    {{-- required --}}
+    pushLocationJs="scripts"
+    {{-- not required this is the palce where you will save and load you url--}}
+    saveUrl="{{'/blog/category/save/'.'your_model_id'}}"
+    {{-- not required this is the palce where you will save and load you url--}}
+    loadUrl="{{'/blog/category/load/'.'your_model_id'}}"
+>
+    {{-- set your style like boostrap or this case tailwind so the brower can render inside the componente --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</x-onix::grapeBuilder>
--- src/Views/components/grapeBuilder.blade.php
+++ src/Views/components/grapeBuilder.blade.php
@@ -1,4 +1,28 @@
+{{-- Check if the push css empty if yes them just display it may not work correct so i recoment use the push --}}
+@if (empty($pushLocationCss))
+    {{-- pushh the css need for this plugisn to work  --}}
+    <x-onix::BuilderCss/>
+@else
+    @push($pushLocationCss)
+        {{-- pushh the css need for this plugisn to work  --}}
+        <x-onix::BuilderCss/>
+    @endpush
+@endif
+
 {{-- start the editor using the id --}}
 <div id="onix-grape">
     {{ $slot }}
 </div>
+
+{{-- Check if the push js empty if yes them just display it may not work correct so i recoment use the push --}}
+@if (empty($pushLocationJs))
+    {{-- pushh the css need for this plugisn to work  --}}
+    <x-onix::BuilderCss/>
+@else
+    @push($pushLocationJs)
+        <x-onix::GrapeBuilderJs
+            saveUrl="{{ $saveUrl ?? null }}"
+            loadUrl="{{ $loadUrl ?? null }}"
+            />
+    @endpush
+@endif
