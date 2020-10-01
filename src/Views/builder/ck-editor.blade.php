10f9db31ae07b472cc109433ba1f6bcaf7361c4c
mario jose goes tarosso <mariojgt2@gmail.com>
2020-10-01 12:33:09 +0100

    view chagne to a now captial letter

--- src/Views/builder/ck-editor.blade.php
+++
@@ -1,8 +0,0 @@
-
-{{-- you can you a push in the componente if need to stack your css or js --}}
-<x-onix::builder-css />
-    <textarea id="editor">
-        <p>Here goes the initial content of the editor.</p>
-    </textarea>
-{{-- you can you a push in the componente if need to stack your css or js --}}
-<x-onix::ckeditor-builder-js />
