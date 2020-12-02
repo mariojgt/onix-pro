905dd7466d56515743be051bea49f6b21796de87
Mario Tarosso <mariojgt2@gmmail.com>
2020-12-02 13:42:35 +0000

    changes

--- src/views/components/ckeditor-builder-js.blade.php
+++ src/views/components/ckeditor-builder-js.blade.php
@@ -7,6 +7,16 @@
 
     ClassicEditor
     .create( document.querySelector( '.editor' ), {
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading2' },
+                { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading2' },
+                { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading2' },
+                { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading2' },
+            ]
+        },
         extraPlugins: [ MyCustomUploadAdapterPlugin ],
         image: {
 			upload: {
